43ff5c54fc5b12412dd4400dd716b390118f3d23
Louisjuc <louis.juchem@proton.me>
2026-07-25 18:13:19 +0200

    path fixed

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -83,20 +83,20 @@ class Character extends moveableObject {
 
   IMAGES_SLEEP = [
     "./img/1.Sharkie/2.Long_IDLE/i1.png",
-    "./img/1.Sharkie/2.Long_IDLE/i2.png",
-    "./img/1.Sharkie/2.Long_IDLE/i3.png",
-    "./img/1.Sharkie/2.Long_IDLE/i4.png",
-    "./img/1.Sharkie/2.Long_IDLE/i4.png",
-    "./img/1.Sharkie/2.Long_IDLE/i5.png",
-    "./img/1.Sharkie/2.Long_IDLE/i6.png",
-    "./img/1.Sharkie/2.Long_IDLE/i7.png",
-    "./img/1.Sharkie/2.Long_IDLE/i8.png",
-    "./img/1.Sharkie/2.Long_IDLE/i9.png",
-    "./img/1.Sharkie/2.Long_IDLE/i10.png",
-    "./img/1.Sharkie/2.Long_IDLE/i11.png",
-    "./img/1.Sharkie/2.Long_IDLE/i12.png",
-    "./img/1.Sharkie/2.Long_IDLE/i13.png",
-    "./img/1.Sharkie/2.Long_IDLE/i14.png"
+    "./img/1.Sharkie/2.Long_IDLE/I2.png",
+    "./img/1.Sharkie/2.Long_IDLE/I3.png",
+    "./img/1.Sharkie/2.Long_IDLE/I4.png",
+    "./img/1.Sharkie/2.Long_IDLE/I4.png",
+    "./img/1.Sharkie/2.Long_IDLE/I5.png",
+    "./img/1.Sharkie/2.Long_IDLE/I6.png",
+    "./img/1.Sharkie/2.Long_IDLE/I7.png",
+    "./img/1.Sharkie/2.Long_IDLE/I8.png",
+    "./img/1.Sharkie/2.Long_IDLE/I9.png",
+    "./img/1.Sharkie/2.Long_IDLE/I10.png",
+    "./img/1.Sharkie/2.Long_IDLE/I11.png",
+    "./img/1.Sharkie/2.Long_IDLE/I12.png",
+    "./img/1.Sharkie/2.Long_IDLE/I13.png",
+    "./img/1.Sharkie/2.Long_IDLE/I14.png"
   ];
 
   IMAGES_BUBBLE = ["./img/1.Sharkie/4.Attack/Bubble trap/Bubble.png"];
